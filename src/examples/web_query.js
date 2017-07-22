efd775160e0525876bc732ff00164ae87a0ec2be
Chris Pearson <chrispsn@gmail.com>
2017-07-22 22:19:03 +1000

    Updated attachment mechanism

--- src/examples/web_query.js
+++ src/examples/web_query.js
@@ -19,11 +19,14 @@ const filter_on = true;
 const filter_fn = (p) => !filter_on || p.id === post_ID;
 const filtered_posts = posts.filter(filter_fn);
 
-const display_fns = require('./display.js').display_fns;
+const {display_fns} = require('./display.js');
 
 if (require.main === module && typeof Mesh !== 'undefined') {
-    Mesh.attach("posts", filtered_posts, [4, 0], display_fns.records_ro);
-  	Mesh.attach("post_ID", post_ID, [0, 0]);
-    Mesh.attach("filter_on", filter_on, [1, 0]);
-    Mesh.attach("filter_fn", filter_fn, [2, 0]);
+  const MESH_ATTACHMENTS = [
+    {id: "posts", value: filtered_posts, loc: [4, 0], display_fn: display_fns.records_ro},
+    {id: "post_ID", value: post_ID, loc: [0, 0]},
+    {id: "filter_on", value: filter_on, loc: [1, 0]},
+    {id: "filter_fn", value: filter_fn, loc: [2, 0]},
+  ];
+  Mesh.attach(MESH_ATTACHMENTS);
 }
