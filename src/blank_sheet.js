e2eaea1525dbdcc7e2e1f260faf8149b5d717c4b
Chris Pearson <chrispsn@gmail.com>
2017-08-19 23:33:40 +1000

    Removed blank sheet file

--- src/blank_sheet.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-// Put your Mesh.attach code in these brackets
-// if you need it to run without Mesh
-if (require.main === module && typeof Mesh !== 'undefined') {
-    const MESH_ATTACHMENTS = [
-    ];
-    Mesh.attach(MESH_ATTACHMENTS);
-}
