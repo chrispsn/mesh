5bbbcf1e9b353d9b3192bbb147ec4649f170299c
Chris Pearson <chrispsn@users.noreply.github.com>
2018-01-18 11:32:44 +1100

    Updated table prototype to allow for generator columns

--- src/__tests__/prototypes.test.js
+++ src/__tests__/prototypes.test.js
@@ -1,5 +1,14 @@
 const Prototypes = require('../prototypes.js');
 
+function* take(n, iterable) {
+    for (const x of iterable) {
+        if (n <= 0) return;
+        n--;
+        yield x;
+    }
+}
+
+
 // TODO should we look at cases where not all columns are the same length?
 describe('TableObject', () => {
     it('deals with a table with no headings or data', () => {
@@ -10,7 +19,6 @@ describe('TableObject', () => {
     it('deals with a table with headings but no data', () => {
         const table = {__proto__: Prototypes.TableObject, single_key: []};
         expect([...table].length).toBe(0);
-        expect(Object.keys(table).length).toBe(1);
     });
     it('gets all records of a table with data', () => {
         const table = {
@@ -22,5 +30,113 @@ describe('TableObject', () => {
         expect(records.length).toBe(2);
         expect(records[0].year).toBe(2002);
         expect(records[1].car).toBe('Audi');
+    });
+    it('handles generator columns', () => {
+        const table = {
+            __proto__: Prototypes.TableObject,
+            number: (function* () { yield 1; yield 2; yield 3; })(),
+        }
+        const records = [...table];
+        expect(records.length).toBe(3);
+        expect(records[1].number).toBe(2);
+        expect(records[0].number).toBe(1);
+    });
+    it('handles cells in columns that reference prior cells in that column', () => {
+        const table = {
+            __proto__: Prototypes.TableObject,
+            get number() { 
+                // Note to readers:
+                // this 't' pattern is used for convenience in testing,
+                // but in a real Mesh sheet you could just use an
+                // explicit ref to the table:
+                // sheet.table[i-1]
+                const t = this; 
+                return (function* () {
+                    yield 1;
+                    for (let i = 1; true; i++) {
+                        yield t[i - 1].number + 1;
+                    }
+                })();
+            },
+        }
+        const [...records] = take(10, table);
+        expect(records.length).toBe(10);
+        expect(records[5].number).toBe(6);
+    });
+    it('handles row cells that use other cells in that row which may be uncomputed', () => {
+        const table = {
+            __proto__: Prototypes.TableObject,
+            get power() { 
+                const t = this; 
+                return (function* () {
+                    for (let i = 0; true; i++) {
+                        yield t[i].number ** 2;
+                    }
+                })();
+            },
+            get number() { 
+                const t = this; 
+                return (function* () {
+                    yield 1;
+                    for (let i = 1; true; i++) {
+                        yield t[i - 1].number + 1;
+                    }
+                })();
+            },
+        }
+        const [...records] = take(10, table);
+        expect(records.length).toBe(10);
+        expect(records[5].power).toBe(36);
+    });
+    it('handles multiple references to the same cell', () => {
+        const table = {
+            __proto__: Prototypes.TableObject,
+            get power() { 
+                const t = this; 
+                return (function* () {
+                    for (let i = 0; true; i++) {
+                        yield t[i].number ** 2;
+                    }
+                })();
+            },
+            get power2() { 
+                const t = this; 
+                return (function* () {
+                    for (let i = 0; true; i++) {
+                        yield t[i].number ** 2;
+                    }
+                })();
+            },
+            get number() { 
+                const t = this; 
+                return (function* () {
+                    yield 1;
+                    for (let i = 1; true; i++) {
+                        yield t[i - 1].number + 1;
+                    }
+                })();
+            },
+        }
+        const [...records] = take(10, table);
+        expect(records.length).toBe(10);
+        expect(records[5].power).toBe(36);
+        expect(records[5].power2).toBe(36);
+    });
+    it('handles data literal columns and computed columns', () => {
+        const table = {
+            __proto__: Prototypes.TableObject,
+            get power() { 
+                const t = this; 
+                return (function* () {
+                    for (let i = 0; true; i++) {
+                        yield t[i].number ** 2;
+                    }
+                })();
+            },
+            get number() { return [1, 2, 3, 4, 5]; }
+        }
+        const [...records] = table;
+        expect(records.length).toBe(5);
+        expect(records[2].power).toBe(9);
     });
 })
--- src/prototypes.js
+++ src/prototypes.js
@@ -1,18 +1,60 @@
 const TableObject = {
-    // TODO Add array-like functions such as filter, map, reduce?
     [Symbol.iterator]: function* () {
-        const headings = Object.keys(this);
-        if (headings.length === 0) return [];
-        let idx = 0;
-        for (let _ of this[headings[0]]) {
-            const obj = {};
-            for (let heading of headings) {
-                obj[heading] = this[heading][idx];
+        const orig_headings = Object.keys(this);
+        // Get iterator equivalents of each column
+        const iterators = {};
+        const GeneratorFunction = Object.getPrototypeOf(function*(){}).constructor;
+        for ([heading, iterable] of Object.entries(this)) {
+            let iterator = iterable[Symbol.iterator]();
+            iterators[heading] = iterator;
+        }
+        // Delete original headings - access the table props via [row index][col name] from here
+        for (let h of orig_headings) { delete this[h] }
+        // Zero columns case
+        if (Object.keys(iterators).length === 0) {return}
+        // Generate rows ( {heading: value} )
+        let index = 0;
+        let done = false;
+        for (;;) {
+            let row = {};
+            // Remember past rows
+            this[index] = row;
+            // Set up property access on this row so that we only
+            // yield from the relevant column when someone tries
+            // to access that row property; when that happens,
+            // cache that value and don't try to yield from that
+            // column again for this row
+            for (let [h, iterator] of Object.entries(iterators)) {
+                Object.defineProperty(row, h, {
+                    enumerable: true, // So the prop will show up in Object.keys
+                    configurable: true, // So we can delete the prop later
+                    get() {
+                        delete this[h];
+                        // TODO should we pass the current index through via next?
+                        // Would it make it easier to write generators?
+                        const item = iterator.next();
+                        if (item.done) {
+                            done = true;
+                            return;
+                        } else {
+                            this[h] = item.value;
+                            return item.value;
+                        }
+                    }
+                });
             }
-            yield obj;
-            idx++;
-        };
-    },
+            // Trigger access for all props in row
+            for (let h of orig_headings) { row[h] /* nothing */ }
+            if (done) {
+                // Delete last row - the last iteration was unsuccessful
+                delete this[index];
+                return;
+            } else {
+                yield row;
+                index = index + 1;
+            }
+        }
+    }
 }
 
 module.exports = {TableObject};
